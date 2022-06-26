a521218476eabec156a7be007066662d81d4c399
s0LA1337 <dreamer@neoncity.dev>
2022-06-26 18:32:55 +0000

    Update

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const pkgs = @import("deps.zig").pkgs;
 
 pub fn build(b: *std.build.Builder) void {
     // Standard target options allows the person running `zig build` to choose
@@ -12,9 +13,9 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("twisted-stock-bot", "src/main.zig");
-    exe.addPackagePath("zCord", "lib/zCord/src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
+    pkgs.addAllTo(exe);
     exe.install();
 
     const run_cmd = exe.run();
--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,30 @@
 const std = @import("std");
+const json = std.json;
+const zcord = @import("zCord");
+
+const secrets = @embedFile("../secrets/secrets.json");
+
+const Config = struct {
+    vals: struct {
+        secret: []const u8,
+        torn_api: []const u8,
+    },
+    uptime: u64,
+};
+
+const config = x: {
+    var stream = json.TokenStream.init(secrets);
+    const res = json.parse(Config, &stream, .{});
+    
+    break :x res catch unreachable;
+};
 
 pub fn main() !void {
+    try zcord.root_ca.preload(std.heap.page_allocator);
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    
+    var auth_buf: [0x100]u8 = undefined;
+    _ = try std.fmt.bufPrint(&auth_buf, "Bot {s}", .{config.vals.secret});
 }
