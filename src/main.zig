7c80fae564e27445f8c4315fa92e31a4793fa002
s0LA1337 <dreamer@neoncity.dev>
2022-06-26 20:42:34 +0000

    Simple additions

--- src/main.zig
+++ src/main.zig
@@ -5,19 +5,15 @@ const zcord = @import("zCord");
 const secrets = @embedFile("../secrets/secrets.json");
 
 const Config = struct {
-    vals: struct {
-        secret: []const u8,
-        torn_api: []const u8,
-    },
-    uptime: u64,
+    token: []const u8,
+    torn_api: []const u8,
 };
 
-const config = x: {
-    var stream = json.TokenStream.init(secrets);
-    const res = json.parse(Config, &stream, .{});
-    
-    break :x res catch unreachable;
-};
+fn get_secrets() !Config {
+    comptime var stream = json.TokenStream.init(secrets);
+    return try comptime json.parse(Config, &stream, .{});
+}
+
 
 pub fn main() !void {
     try zcord.root_ca.preload(std.heap.page_allocator);
@@ -25,6 +21,32 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     
+    const config = try comptime get_secrets();
     var auth_buf: [0x100]u8 = undefined;
-    _ = try std.fmt.bufPrint(&auth_buf, "Bot {s}", .{config.vals.secret});
+    const auth = try std.fmt.bufPrint(&auth_buf, "Bot {s}", .{config.token});
+
+    const client = zcord.Client {
+        .auth_token = auth,
+    };
+    
+    const gateway = try client.startGateway(.{
+        .allocator = gpa.allocator(),
+        .intents = .{ .guild_messages = true },
+    });
+    defer gateway.destroy();
+    
+    while(true) {
+        const event = try gateway.recvEvent();
+        defer event.deinit();
+        
+        processEvent(event) catch |err| {
+            std.debug.print("Event error: {}\n", .{err});
+        };
+    }
+}
+
+fn processEvent(event: zcord.Gateway.Event) !void {
+    switch (event.name) {
+        else => {},
+    }
 }
