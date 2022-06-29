a373cbabe2e747ece2c37aedcd229318fa195a9c
Sora <dream@neoncity.dev>
2022-06-29 12:20:42 +0200

    Update build.zig

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("twisted-stock-bot", "src/main.zig");
+    const exe = b.addExecutable("torn-stock-bot", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     pkgs.addAllTo(exe);
